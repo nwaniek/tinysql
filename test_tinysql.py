613d48e061131ca0ad00f3d5d81579ec45f7baf0
Nicolai Waniek <rochus@rochus.net>
2024-08-22 17:22:21 +0200

    insert and tests

---
+++ test_tinysql.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from enum import Enum
+from typing import NamedTuple
+import tinysql
+import numpy as np
+
+@tinysql.db_table("AmazingValues", primary_keys=["id"])
+class AmazingValues(NamedTuple):
+    id:     int
+    value0: str
+    value1: float
+    value2: np.ndarray
+
+
+@tinysql.db_enum("MyEnum", descriptions={'One': 'First field of MyEnum', 'Two': 'Second field of MyEnum'})
+class MyEnum(Enum):
+    One = "one"
+    Two = "two"
+
+
+def test_insert(context):
+    print("Table 'AmazingValues' exists:", tinysql.table_exists(context.con, "AmazingValues"))
+
+    # insert via object
+    values = AmazingValues(1, "hello, world!", 123.12, np.ones((4, 4)))
+    tinysql.insert(context, values)
+
+    # insert as a dict with corresponding tablespec
+    values = {'id': 2,
+              'value0': 'world, hello!',
+              'value1': 71.71,
+              'value2': np.eye(10)}
+    tinysql.insert(context, values, tinysql.get_tspec(AmazingValues))
+
+    # print everything in the table
+    sql = "SELECT * FROM AmazingValues"
+    rows = context.con.execute(sql)
+    for row in rows:
+        print(row)
+
+
+def test_enum(context):
+    print("Table 'MyEnum exists:", tinysql.table_exists(context.con, "MyEnum"))
+    sql = "SELECT value, name, description FROM MyEnum"
+    rows = context.con.execute(sql)
+    for row in rows:
+        print(f"MyEnum.{row[1]} = '{row[0]}' # {row[2]}")
+
+
+if __name__ == "__main__":
+    with tinysql.setup_db('test.sqlite', 'test_storage') as context:
+        test_insert(context)
+        test_enum(context)
--- tinysql.py
+++ tinysql.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 from types import SimpleNamespace
-from typing import NamedTuple, Tuple, Callable, List, Dict, get_type_hints, Type
+from typing import NamedTuple, Tuple, Callable, List, Dict, Union, get_type_hints, Type
 import io
 import sqlite3
 import pickle
@@ -65,8 +65,8 @@ def db_table(tablename: str, primary_keys: List[str] | None = None, foreign_keys
         if tablename in TABLE_REGISTRY:
             raise ValueError(f"Duplicate table name detected: {tablename}")
 
-        cls._tinysql_table_spec = ts
-        cls._tinysql_init_fn    = init_fn
+        cls._tinysql_tspec   = ts
+        cls._tinysql_init_fn = init_fn
         TABLE_REGISTRY[tablename] = TableRegistryEntry(ts, init_fn, cls)
         return cls
     return decorator
@@ -97,12 +97,24 @@ def db_enum(tablename: str, descriptions: Dict[str, str] = {}):
         ts.fields.description = "TEXT"
         ts.primary_keys       = ["value"]
 
-        cls._tinysql_table_spec = ts
-        cls._tinysql_init_fn    = db_enum_initfn(tablename, [(f.value, f._name_, descriptions.get(f._name_, "")) for f in cls])
+        if tablename in TABLE_REGISTRY:
+            raise ValueError(f"Duplicate table name detected: {tablename}")
 
+        cls._tinysql_tspec   = ts
+        cls._tinysql_init_fn = db_enum_initfn(tablename, [(f.value, f._name_, descriptions.get(f._name_, "")) for f in cls])
+        TABLE_REGISTRY[tablename] = TableRegistryEntry(ts, cls._tinysql_init_fn, cls)
         return cls
-
     return decorator
+
+
+def get_tspec(cls: Type):
+    if hasattr(cls, '_tinysql_tspec'):
+        return cls._tinysql_tspec
+    else:
+        tspec = TABLE_REGISTRY.get(cls.__name__, None)
+        if tspec is not None:
+            return tspec
+        raise RuntimeError(f"Type not mapped to database: {cls}.")
 
 
 def sql_builder_mk_table(tspec: TableSpec):
@@ -118,15 +130,16 @@ def sql_builder_mk_table(tspec: TableSpec):
 
 def sql_builder_insert(tspec: TableSpec, replace_existing = False):
     modifier = "or REPLACE" if replace_existing else "" # "or IGNORE"
-    sql = "INSERT {} INTO {}".format(modifier, tspec.name)
+    sql = f"INSERT {modifier} INTO {tspec.name}"
     sql += "("
     sql += ", ".join(key for key in tspec.fields.__dict__.keys())
     sql += ") VALUES ("
     sql += ", ".join("?" for _ in range(len(tspec.fields.__dict__)))
     sql += ");"
+    return sql
 
 
-def dump(context, tspec, fieldname, data, obj):
+def dump(context, tspec, fieldname, data, obj, get_fn):
     if obj is None:
         return ""
 
@@ -135,7 +148,7 @@ def dump(context, tspec, fieldname, data, obj):
     tspec_dir = Path(tspec.name)
     (root / tspec_dir).mkdir(parents=True, exist_ok=True)
 
-    fname = "_".join(str(data[key]) for key in tspec.primary_keys)
+    fname = "_".join(str(get_fn(data, key)) for key in tspec.primary_keys)
     fname = "_".join([fname, fieldname])
 
     if type(obj) == np.ndarray:
@@ -150,22 +163,36 @@ def dump(context, tspec, fieldname, data, obj):
     return str(tspec_dir / fname)
 
 
-def insert(context, data, tspec: TableSpec):
+def insert_impl(context, data, tspec: TableSpec, get_fn: Callable):
     sql = sql_builder_insert(tspec)
     _data = tuple()
 
     # build the data for the sqlite table
     for fname, ftype in tspec.fields.__dict__.items():
         if context.use_external_storage and ftype.lower() in ['blob', 'ndarray']:
-            fpath = dump(context, tspec, fname, data, data[fname])
+            fpath = dump(context, tspec, fname, data, get_fn(data, fname), get_fn)
             _data = _data + (fpath, )
         else:
-            _data = _data + (data[fname], )
+            _data = _data + (get_fn(data, fname), )
 
     # finally execute
     cur = context.con.cursor()
     cur.execute(sql, _data)
     context.con.commit()
+
+
+def insert(context, data, tspec: TableSpec | None = None):
+    if hasattr(data, '_tinysql_tspec'):
+        insert_impl(context, data, data._tinysql_tspec, lambda d, k: getattr(d, k))
+
+    elif isinstance(data, dict):
+        if tspec is not None:
+            insert_impl(context, data, tspec, lambda d, k: d[k])
+        else:
+            raise RuntimeError(f"TableSpec missing")
+
+    else:
+        raise RuntimeError(f"Type not mapped to database: {type(data)}")
 
 
 def table_exists(con, tablename):
@@ -209,7 +236,10 @@ class DatabaseContext:
         self.close()
 
 
-def setup_db(db_path: Path, table_storage_root: Path | None):
+def setup_db(db_path: Path | str, table_storage_root: Path | str | None):
+    db_path = Path(db_path) if isinstance(db_path, str) else db_path
+    table_storage_root = Path(table_storage_root) if isinstance(table_storage_root, str) else table_storage_root
+
     context = DatabaseContext(db_path, table_storage_root)
     cur = context.con.cursor()
     for tabledef in TABLE_REGISTRY.values():
